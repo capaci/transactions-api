eaf2787d6d3b5cf4d17face6830b8aaf281a8da8
Rafael Capaci <rafaelcapacipereira@gmail.com>
2022-01-28 15:49:05 -0300

    Add fake data to tests

---
+++ tests/data/transactions.ts
@@ -0,0 +1,28 @@
+import { ITransaction } from "../../src/useCases/interfaces";
+
+const transactions: ITransaction[] = [
+    {
+        id: 1,
+        amount: 100,
+        date: new Date(),
+        description: 'Some description',
+        method: 'debit_card',
+        card_number: '1234123412341234',
+        card_owner_name: 'John Doe',
+        card_expiration_date: '12/2030',
+        card_cvv: '123'
+    },
+    {
+        id: 2,
+        amount: 200,
+        date: new Date(),
+        description: 'Some description',
+        method: 'credit_card',
+        card_number: '2341234123412345',
+        card_owner_name: 'John Doe',
+        card_expiration_date: '11/2030',
+        card_cvv: '321'                
+    }
+]
+
+export default transactions;
--- tests/useCases/listTransactions.test.ts
+++ tests/useCases/listTransactions.test.ts
@@ -1,31 +1,6 @@
 import { TransactionRepositoryInMemory } from "../../src/repositories/transactionRepositoryInMemory";
-import { ITransaction } from "../../src/useCases/interfaces";
 import { ListTransactions } from "../../src/useCases/listTransactions";
-
-const transactions: ITransaction[] = [
-    {
-        id: 1,
-        amount: 100,
-        date: new Date(),
-        description: 'Some description',
-        method: 'debit_card',
-        card_number: '1234123412341234',
-        card_owner_name: 'John Doe',
-        card_expiration_date: '12/2030',
-        card_cvv: '123'
-    },
-    {
-        id: 2,
-        amount: 200,
-        date: new Date(),
-        description: 'Some description',
-        method: 'credit_card',
-        card_number: '2341234123412345',
-        card_owner_name: 'John Doe',
-        card_expiration_date: '11/2030',
-        card_cvv: '321'                
-    }
-]
+import fakeTransactions from "../data/transactions";
 
 
 describe('Test transactions listing use case', () => {
@@ -45,12 +20,12 @@ describe('Test transactions listing use case', () => {
     test('should return full list of transactions', async () => {
         const transactionRepository = new TransactionRepositoryInMemory();
 
-        let spy = jest.spyOn(transactionRepository, 'getAll').mockReturnValue(Promise.resolve(transactions));
+        let spy = jest.spyOn(transactionRepository, 'getAll').mockReturnValue(Promise.resolve(fakeTransactions));
         const listTransactionUC = new ListTransactions(transactionRepository);
 
         const result = await listTransactionUC.execute();
 
-        expect(result).toEqual(transactions);
+        expect(result).toEqual(fakeTransactions);
 
         spy.mockRestore()
     })
